6488ab96043fa6ff49f81f5c3508897e420f7fcf
Alber Tenez <albert@sabatemartinez.com>
2019-02-25 13:45:20 +0800

    Add enzyme and another test to loading

--- src/components/loading/Loading.test.js
+++ src/components/loading/Loading.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Loading from './Loading';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Loading />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('#Loading', () => {
+  it('should renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Loading />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render the exact schema', () => {
+    const actual = shallow(
+      <Loading />
+    );
+    const expected = (
+      <div className="loading">
+        <div className="loading-bounce"></div>
+        <div className="loading-bounce"></div>
+      </div>
+    );
+    expect(actual.matchesElement(expected)).toBeTruthy();
+  });
 });
