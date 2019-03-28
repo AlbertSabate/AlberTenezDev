b94f5d639ac166342bdc7b2d595a0254eab1005d
Alber Tenez <albert@sabatemartinez.com>
2019-03-28 15:05:24 +0800

    Fixing broken test

--- src/components/about/About.test.js
+++ src/components/about/About.test.js
@@ -5,7 +5,7 @@ import About from './About';
 describe('#About', () => {
   it('should renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<About />, div);
+    ReactDOM.render(<About info={{}} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
