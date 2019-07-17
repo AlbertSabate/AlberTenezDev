187eb3f64f33bede1b05cea0861fceafa4e31ec8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-17 07:24:43 +0000

    Deploy AlbertSabate/AlberTenezDev to github.com/AlbertSabate/AlberTenezDev.git:gh-pages

---
+++ precache-manifest.ab39668ff6697a1b62559eb4ee25f58f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34529e323a589caadf9edd66fec5f257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f5782f26d4fc7806a34",
+    "url": "/static/css/main.2373b9f4.chunk.css"
+  },
+  {
+    "revision": "031b422d3e1932b6439a",
+    "url": "/static/js/2.a28fbd18.chunk.js"
+  },
+  {
+    "revision": "5f5782f26d4fc7806a34",
+    "url": "/static/js/main.5e13a057.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4cc116987a7072de9354f5381a12f0b1",
+    "url": "/static/media/bg-original-min.4cc11698.jpg"
+  }
+]);
